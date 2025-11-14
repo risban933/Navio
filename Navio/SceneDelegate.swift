33cf439bf7a963411cab553bd4d2305f05f54465
Rishabh Bansal <rishabh.rb@icloud.com>
2025-11-13 20:43:08 -0800

    Initial commit

---
+++ Navio/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Navio
+//
+//  Created by Rishabh Bansal on 11/13/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
---
+++ NavioTests/NavioTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavioTests.swift
+//  NavioTests
+//
+//  Created by Rishabh Bansal on 11/13/25.
+//
+
+import Testing
+@testable import Navio
+
+struct NavioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
